ab23b46e70d4bf237cc7a6f0b6385d41b882d5c3
Alexey Fedoseev <aleksey@fedoseev.net>
2026-08-16 00:22:03 +0300

    copyright string

--- tests/04-read-berloga.c
+++ tests/04-read-berloga.c
@@ -3,7 +3,7 @@
  *
  * The legacy Berloga document reading test
  *
- * Copyright (C) 2026 Alexey Fedoseev <aleksey@fedoseev.net>
+ * Copyright (C) 2024-2026 Alexey Fedoseev <aleksey@fedoseev.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
